82f2afd33d9bd4c34ef69644353a129e789f84b5
veffhz <veffhz@example.com>
2020-01-21 21:23:04 +0300

    Added number format, strong font

--- application/filters.py
+++ application/filters.py
@@ -11,3 +11,8 @@ def limit(dictionary, n):
 @app.template_filter('split')
 def split(items, key, n):
     return items.get(key).split()[n]
+
+
+@app.template_filter('thousand')
+def thousand(price):
+    return f'{price:,d}'.replace(',', ' ')
