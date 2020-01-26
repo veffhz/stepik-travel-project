51a0c4b2bc8db56d651d70c54bed5fd2e813b00f
veffhz <veffhz@example.com>
2020-01-26 21:52:36 +0300

    Fixed filters functions.

--- application/filters.py
+++ application/filters.py
@@ -5,14 +5,25 @@ from flask import current_app as app
 
 @app.template_filter('limit')
 def limit(dictionary, n):
+    if len(dictionary) < n:
+        return random.sample(dictionary.items(), len(dictionary))
     return random.sample(dictionary.items(), n)
 
 
 @app.template_filter('split')
 def split(items, key, n):
-    return items.get(key).split()[n]
+    item = items.get(key)
+    if item:
+        return items.get(key).split()[n]
+    else:
+        return key
 
 
 @app.template_filter('thousand')
 def thousand(price):
-    return f'{price:,d}'.replace(',', ' ')
+    if price and isinstance(price, int):
+        return f'{price:,d}'.replace(',', ' ')
+    elif isinstance(price, float):
+        return f'{price:.3f}'.replace('.', ' ')
+    else:
+        return f'{price}'
